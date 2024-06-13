8706470da483a3e515084e14ed338dd37bb0e13a
Weest0 <wesleyusumaki910@gmail.com>
2024-06-13 16:09:46 -0300

    Novas Funcionalidades: Atualizar e Deletar.

--- script/principal.js
+++ script/principal.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para deixar um pouco mais fluido.
     const formManga = document.getElementById('form-manga');
     const listaMangas = document.getElementById('lista-mangas');
+    const overlay = document.getElementById('overlay');
+    const botaoFecharFormulario = document.getElementById('fechar');
     let vetorMangas = [];
+    let emQualEsta = null;
 
     class Manga {
         constructor(titulo, autor, ultimoCapituloLido, linkImagem, genero, status, categoria) {
@@ -17,13 +20,57 @@ document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para de
         
     };
 
+    botaoFecharFormulario.addEventListener('click', () => {
+        formManga.style.display = 'none';
+        overlay.style.display = 'none';
+        abrirFormularioAux =! abrirFormularioAux;
+    })
+
+    function configurarBotao(button, nome){
+        button.value = nome;
+        button.type = 'button';
+    };
+
+    function deletarManga(index){
+        vetorMangas.splice(index, 1);
+        exibirListaMangas();
+    }
+
+    function editarManga(index){
+        const manga = vetorMangas[index];
+
+        document.getElementById('campo-titulo').value = manga.titulo;
+        document.getElementById('campo-autor').value = manga.autor;
+        document.getElementById('campo-por-ultimo').value = manga.ultimoCapituloLido;
+        document.getElementById('campo-imagem').value = manga.linkImagem;
+        document.getElementById('genero').value = manga.genero;
+        document.getElementById('status').value = manga.status;
+        document.getElementById('categoria').value = manga.categoria;
+
+        emQualEsta = index;
+        formManga.style.display = 'flex';
+        overlay.style.display = 'flex';
+        abrirFormularioAux = false;
+    }
+
+    function classesTexto(titulo, autor, ultimoCapituloLido, generoECategoria, status){
+        titulo.className = 'label-titulo';
+        autor.className = 'label-autor';
+        ultimoCapituloLido.className = 'label-capitulo';
+        generoECategoria.className = 'label-genero-categoria';
+        status.className = 'label-status';
+    }
+
+
     function exibirListaMangas(){
         listaMangas.innerHTML = '';
-        vetorMangas.forEach(manga => {
+        vetorMangas.forEach((manga, index) => {
             const imagem = document.createElement('img');
             const card = document.createElement('div');
+            const cardLadoDireito = document.createElement('div');
             const cardImagem = document.createElement('div');
             const cardTexto = document.createElement('div');
+            const cardBotao = document.createElement('div');
 
             const labelTitulo = document.createElement('label');
             const labelAutor = document.createElement('label');
@@ -31,28 +78,48 @@ document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para de
             const labelGeneroECategoria = document.createElement('label');
             const labelStatus = document.createElement('label');
 
+            const botaoEditar = document.createElement('input');
+            botaoEditar.addEventListener('click', () => editarManga(index));
+            const botaoDeletar = document.createElement('input');
+            botaoDeletar.addEventListener('click', () => deletarManga(index));
+
+            configurarBotao(botaoEditar, 'Editar');
+            configurarBotao(botaoDeletar, 'Deletar');
+
+            (manga.status === 'Lendo') ? labelStatus.style.backgroundColor = 'green' : labelStatus.style.backgroundColor = 'red'; 
+
             labelTitulo.innerHTML = `Titulo: ${manga.titulo}`;
             (manga.autor === '') ? labelAutor.innerHTML = 'Autor: Desconhecido' : labelAutor.innerHTML = `Autor: ${manga.autor}`;
             labelUltimoCapLido.innerHTML = `Ultimo capitulo lido: ${manga.ultimoCapituloLido}`;
             labelGeneroECategoria.innerHTML = `${manga.genero} · ${manga.categoria}`;
             labelStatus.innerHTML = `${manga.status}`;
-            (manga.linkImagem === '') ? imagem.src = 'images/erro.png' : imagem.src = manga.linkImagem;
+            (manga.linkImagem === '') ? imagem.src = '' : imagem.src = manga.linkImagem;
 
+            
             cardTexto.appendChild(labelTitulo);
             cardTexto.appendChild(labelAutor);
             cardTexto.appendChild(labelGeneroECategoria);
-            cardTexto.appendChild(labelStatus);
             cardTexto.appendChild(labelUltimoCapLido);
+            cardTexto.appendChild(labelStatus);
+            cardLadoDireito.appendChild(cardTexto);
+            cardLadoDireito.appendChild(cardBotao);
             
             cardImagem.appendChild(imagem);
+            cardBotao.appendChild(botaoEditar);
+            cardBotao.appendChild(botaoDeletar);
             card.appendChild(cardImagem);
-            card.appendChild(cardTexto);
+            card.appendChild(cardLadoDireito);
             listaMangas.appendChild(card);
             
             card.className = 'card';
             imagem.className = 'imagem';
             cardImagem.className = 'card-imagem'
             cardTexto.className = 'card-texto';
+            cardLadoDireito.className = 'card-lado-direito';
+            botaoDeletar.className = 'botao-acao';
+            botaoEditar.className = 'botao-acao';
+            cardBotao.className = 'card-botao';
+            classesTexto(labelTitulo, labelAutor, labelUltimoCapLido, labelGeneroECategoria, labelStatus);
         });
     };
 
@@ -77,17 +144,26 @@ document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para de
             categoria.options[categoria.selectedIndex].text
         );
 
-        vetorMangas.push(novoManga);
+        if (emQualEsta === null) {
+            vetorMangas.push(novoManga);
+        } else {
+            vetorMangas[emQualEsta] = novoManga;
+            emQualEsta = null;
+        }
+
         exibirListaMangas();
         formManga.reset();
         formManga.style.display = 'none';
+        overlay.style.display = 'none';
         abrirFormularioAux =! abrirFormularioAux;
         
     });
 
+    
     const botaoAbrirFormulario = document.getElementById('btn-abrir-form');
     let abrirFormularioAux = true;
     botaoAbrirFormulario.addEventListener('click', () => {
+        (abrirFormularioAux) ? overlay.style.display = 'flex' : overlay.style.display = 'none';
         (abrirFormularioAux) ? formManga.style.display = 'flex' : formManga.style.display = 'none';
         abrirFormularioAux =! abrirFormularioAux;
     });
