7ee80e07133d5bd15d925019bf191a75f8979e6d
Weest0 <wesleyusumaki910@gmail.com>
2024-08-05 03:42:31 -0300

    Separação do db dos usuarios

--- script/principal.js
+++ script/principal.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, fetchSignInMethodsForEmail, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
 import chamarPopUpAviso from "./erroPopUp.js";
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -18,15 +19,35 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para deixar um pouco mais fluido.
     const formManga = document.getElementById('form-manga');
+    const formAtualizarManga = document.getElementById('form-atualizar-manga');
     const listaMangas = document.getElementById('lista-mangas');
     const overlay = document.getElementById('overlay');
     const botaoFecharFormulario = document.getElementById('fechar');
+    const botaoFecharAtualizarFormulario = document.getElementById('fechar-atualizar-form');
     const botaoAdicionarManga = document.getElementById('btn-adicionar');
-    let vetorMangas = [];
-    let emQualEsta = null;
+    const botaoSair = document.getElementById('btn-sair-usuario');
+    const nomeUsuario = document.getElementById('nome-usuario');
+    const imgLoading = document.getElementById('carregamento');
+    const textoBotao = document.getElementById('texto-botao');
+    let usuarioGlobal;
+    let aux = true;
+
+    //Parte de atualizar o mangá
+
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            usuarioGlobal = user;
+            exibirListaMangas(user.uid);
+            nomeUsuario.textContent = ' Bem-vindo, ' + user.email;
+        } else {
+            nomeUsuario.textContent = "Usuário não autenticado";
+        }
+    });
 
     class Manga {
         constructor(titulo, autor, ultimoCapituloLido, linkImagem, genero, status, categoria) {
@@ -103,34 +124,16 @@ document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para de
         abrirFormularioAux =! abrirFormularioAux;
     })
 
+    botaoSair.addEventListener('click', () => {
+        signOut(auth);
+        window.location.href = 'index.html';
+    });
+
     function configurarBotao(button, nome){
         button.value = nome;
         button.type = 'button';
     };
 
-    function deletarManga(index){
-        vetorMangas.splice(index, 1);
-        exibirListaMangas();
-    }
-
-    function editarManga(index){
-        const manga = vetorMangas[index];
-
-        document.getElementById('campo-titulo').value = manga.titulo;
-        document.getElementById('campo-autor').value = manga.autor;
-        document.getElementById('campo-por-ultimo').value = manga.ultimoCapituloLido;
-        document.getElementById('campo-imagem').value = manga.linkImagem;
-        document.getElementById('genero').value = manga.genero;
-        document.getElementById('status').value = manga.status;
-        document.getElementById('categoria').value = manga.categoria;
-        botaoFecharFormulario.style.display = 'none';
-        botaoAdicionarManga.textContent = 'Atualizar Mangá';
-
-        emQualEsta = index;
-        formManga.style.display = 'flex';
-        overlay.style.display = 'flex';
-        abrirFormularioAux = false;
-    }
 
     function classesTexto(titulo, autor, ultimoCapituloLido, generoECategoria, status){
         titulo.className = 'label-titulo';
@@ -140,104 +143,206 @@ document.addEventListener('DOMContentLoaded', () => { //DOMContentLoaded para de
         status.className = 'label-status';
     }
 
-
-    function exibirListaMangas(){
+    
+    async function exibirListaMangas(userId){
+        const caminhoColecao = collection(db, 'usuarios', userId, 'mangas');
+        /* const querySnapshot = await getDocs(collection(db, 'usuarios', userId, 'mangas')); */
         listaMangas.innerHTML = '';
-        vetorMangas.forEach((manga, index) => {
-            const imagem = document.createElement('img');
-            const card = document.createElement('div');
-            const cardLadoDireito = document.createElement('div');
-            const cardImagem = document.createElement('div');
-            const cardTexto = document.createElement('div');
-            const cardBotao = document.createElement('div');
 
-            const labelTitulo = document.createElement('label');
-            const labelAutor = document.createElement('label');
-            const labelUltimoCapLido = document.createElement('label');
-            const labelGeneroECategoria = document.createElement('label');
-            const labelStatus = document.createElement('label');
+        const atualizarAutomaticamente = onSnapshot(caminhoColecao, (querySnapshot) => {
+            querySnapshot.forEach((documento) => {
+                const mangaData = documento.data();
+                const manga = new Manga(
+                    mangaData.titulo,
+                    mangaData.autor,
+                    mangaData.ultimoCapituloLido,
+                    mangaData.linkImagem,
+                    mangaData.genero,
+                    mangaData.status,
+                    mangaData.categoria
+                );
+    
+                const imagem = document.createElement('img');
+                const card = document.createElement('div');
+                const cardLadoDireito = document.createElement('div');
+                const cardImagem = document.createElement('div');
+                const cardTexto = document.createElement('div');
+                const cardBotao = document.createElement('div');
+    
+                const labelTitulo = document.createElement('label');
+                const labelAutor = document.createElement('label');
+                const labelUltimoCapLido = document.createElement('label');
+                const labelGeneroECategoria = document.createElement('label');
+                const labelStatus = document.createElement('label');
+    
+                const botaoEditar = document.createElement('input');
+                botaoEditar.addEventListener('click', () => {
+                    const novoCampoTitulo = document.getElementById('novo-campo-titulo');
+                    const novoCampoAutor = document.getElementById('novo-campo-autor');
+                    const novoCampoUltimoLido = document.getElementById('novo-campo-por-ultimo');
+                    const novoCampoLinkImagem = document.getElementById('novo-campo-imagem');
+                    const novaCategoria = document.getElementById('nova-categoria');
+                    const novoStatus = document.getElementById('novo-status');
+                    const novoGenero = document.getElementById('novo-genero');
+                    const caminho = doc(db, 'usuarios', userId, 'mangas', documento.id);
 
-            const botaoEditar = document.createElement('input');
-            botaoEditar.addEventListener('click', () => editarManga(index));
-            const botaoDeletar = document.createElement('input');
-            botaoDeletar.addEventListener('click', () => deletarManga(index));
+                    novoCampoTitulo.value = manga.titulo;
+                    novoCampoAutor.value = manga.autor;
+                    novoCampoUltimoLido.value = manga.ultimoCapituloLido;
+                    novoCampoLinkImagem.value = manga.linkImagem;
+                    novaCategoria.value = manga.categoria;
+                    novoStatus.value = manga.status;
+                    novoGenero.value = manga.genero; 
 
-            configurarBotao(botaoEditar, 'Editar');
-            configurarBotao(botaoDeletar, 'Deletar');
+                    if(aux){
+                        formAtualizarManga.style.display = 'flex';
+                        overlay.style.display = 'flex';
+                    }
+                    
+                    botaoFecharAtualizarFormulario.addEventListener('click', () => {
+                        formAtualizarManga.style.display = 'none';
+                        overlay.style.display = 'none';
+                        aux =! aux
+                    });
 
-            (manga.status === 'Lendo') ? labelStatus.style.backgroundColor = 'green' : labelStatus.style.backgroundColor = 'red'; 
+                    try{
+                        formAtualizarManga.addEventListener('submit', async (event) => {
+                            event.preventDefault();
+                    
+                            let novosDados = {
+                                titulo: novoCampoTitulo.value,
+                                autor: novoCampoAutor.value,
+                                ultimoCapituloLido: novoCampoUltimoLido.value,
+                                linkImagem: novoCampoLinkImagem.value,
+                                genero: novoGenero.value,
+                                status: novoStatus.value,
+                                categoria: novaCategoria.value
+                            }
+                    
+                            await updateDoc(caminho, novosDados);
+                            exibirListaMangas(userId);
+                            formAtualizarManga.style.display = 'none';
+                            overlay.style.display = 'none';
+                        });
+                    } catch(error){
+                        console.error('Error', error.message);
+                        chamarPopUpAviso(error.message, 'red');
+                    }
 
-            labelTitulo.innerHTML = `Titulo: ${manga.titulo}`;
-            (manga.autor === '') ? labelAutor.innerHTML = 'Autor: Desconhecido' : labelAutor.innerHTML = `Autor: ${manga.autor}`;
-            labelUltimoCapLido.innerHTML = `Ultimo capitulo lido: ${manga.ultimoCapituloLido}`;
-            labelGeneroECategoria.innerHTML = `${manga.genero} · ${manga.categoria}`;
-            labelStatus.innerHTML = `${manga.status}`;
-            (manga.linkImagem === '') ? imagem.src = '' : imagem.src = manga.linkImagem;
+                    
+                });
 
-            
-            cardTexto.appendChild(labelTitulo);
-            cardTexto.appendChild(labelAutor);
-            cardTexto.appendChild(labelGeneroECategoria);
-            cardTexto.appendChild(labelUltimoCapLido);
-            cardTexto.appendChild(labelStatus);
-            cardLadoDireito.appendChild(cardTexto);
-            cardLadoDireito.appendChild(cardBotao);
-            
-            cardImagem.appendChild(imagem);
-            cardBotao.appendChild(botaoEditar);
-            cardBotao.appendChild(botaoDeletar);
-            card.appendChild(cardImagem);
-            card.appendChild(cardLadoDireito);
-            listaMangas.appendChild(card);
-            
-            card.className = 'card';
-            imagem.className = 'imagem';
-            cardImagem.className = 'card-imagem'
-            cardTexto.className = 'card-texto';
-            cardLadoDireito.className = 'card-lado-direito';
-            botaoDeletar.className = 'botao-acao';
-            botaoEditar.className = 'botao-acao';
-            cardBotao.className = 'card-botao';
-            classesTexto(labelTitulo, labelAutor, labelUltimoCapLido, labelGeneroECategoria, labelStatus);
+                const botaoDeletar = document.createElement('input');
+                botaoDeletar.addEventListener('click', async () => {
+                    try{
+                        const caminho = doc(db, 'usuarios', userId, 'mangas', documento.id);
+                        await deleteDoc(caminho);
+                        chamarPopUpAviso('Excluido', 'yellow');
+                        exibirListaMangas(userId);
+                    } catch(error){
+                        console.error('Error', error.message);
+                        chamarPopUpAviso(error.message, 'red');
+                    }
+
+                });
+    
+                configurarBotao(botaoEditar, 'Editar');
+                configurarBotao(botaoDeletar, 'Deletar'); 
+    
+                (manga.status === 'Lendo') ? labelStatus.style.backgroundColor = 'green' : labelStatus.style.backgroundColor = 'red'; 
+    
+                labelTitulo.innerHTML = `Titulo: ${manga.titulo}`;
+                (manga.autor === '') ? labelAutor.innerHTML = 'Autor: Desconhecido' : labelAutor.innerHTML = `Autor: ${manga.autor}`;
+                labelUltimoCapLido.innerHTML = `Ultimo capitulo lido: ${manga.ultimoCapituloLido}`;
+                labelGeneroECategoria.innerHTML = `${manga.genero} · ${manga.categoria}`;
+                labelStatus.innerHTML = `${manga.status}`;
+                (manga.linkImagem === '') ? imagem.src = '' : imagem.src = manga.linkImagem;
+    
+                cardTexto.appendChild(labelTitulo);
+                cardTexto.appendChild(labelAutor);
+                cardTexto.appendChild(labelGeneroECategoria);
+                cardTexto.appendChild(labelUltimoCapLido);
+                cardTexto.appendChild(labelStatus);
+                cardLadoDireito.appendChild(cardTexto);
+                cardLadoDireito.appendChild(cardBotao);
+                
+                cardImagem.appendChild(imagem);
+                cardBotao.appendChild(botaoEditar);
+                cardBotao.appendChild(botaoDeletar); 
+                card.appendChild(cardImagem);
+                card.appendChild(cardLadoDireito);
+                listaMangas.appendChild(card);
+                
+                card.className = 'card';
+                imagem.className = 'imagem';
+                cardImagem.className = 'card-imagem'
+                cardTexto.className = 'card-texto';
+                cardLadoDireito.className = 'card-lado-direito';
+                botaoDeletar.className = 'botao-acao';
+                botaoEditar.className = 'botao-acao';
+                cardBotao.className = 'card-botao';
+                classesTexto(labelTitulo, labelAutor, labelUltimoCapLido, labelGeneroECategoria, labelStatus);
+            });
         });
+        
     };
 
-    formManga.addEventListener('submit', (event) => {
+    formManga.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-        const campoTitulo = document.getElementById('campo-titulo');
-        const campoAutor = document.getElementById('campo-autor');
-        const campoUltimoLido = document.getElementById('campo-por-ultimo');
-        const campoLinkImagem = document.getElementById('campo-imagem');
-        const categoria = document.getElementById('categoria');
-        const status = document.getElementById('status');
-        const genero = document.getElementById('genero');
+        setTimeout(() => {
+            textoBotao.display = 'none';
+            imgLoading.display = 'block';
+        }, 3000);
 
-        const novoManga = new Manga(
-            campoTitulo.value,
-            campoAutor.value,
-            campoUltimoLido.value,
-            campoLinkImagem.value,
-            genero.options[genero.selectedIndex].text,
-            status.options[status.selectedIndex].text,
-            categoria.options[categoria.selectedIndex].text
-        );
+        textoBotao.display = 'block';
+        imgLoading.display = 'none';
 
-        if (emQualEsta === null) {
-            vetorMangas.push(novoManga);
+        if(usuarioGlobal){
+            const campoTitulo = document.getElementById('campo-titulo');
+            const campoAutor = document.getElementById('campo-autor');
+            const campoUltimoLido = document.getElementById('campo-por-ultimo');
+            const campoLinkImagem = document.getElementById('campo-imagem');
+            const categoria = document.getElementById('categoria');
+            const status = document.getElementById('status');
+            const genero = document.getElementById('genero');
+
+            const novoManga = new Manga(
+                campoTitulo.value,
+                campoAutor.value,
+                campoUltimoLido.value,
+                campoLinkImagem.value,
+                genero.options[genero.selectedIndex].text,
+                status.options[status.selectedIndex].text,
+                categoria.options[categoria.selectedIndex].text
+            );
+
+            try{
+                await addDoc(collection(db, 'usuarios', usuarioGlobal.uid, 'mangas'), {
+                    titulo: novoManga.getTitulo(),
+                    autor: novoManga.getAutor(),
+                    ultimoCapituloLido: novoManga.getUltimoCapituloLido(),
+                    linkImagem: novoManga.getLinkImagem(),
+                    genero: novoManga.getGenero(),
+                    status: novoManga.getStatus(),
+                    categoria: novoManga.getCategoria()
+                });
+                chamarPopUpAviso('Adicionado com sucesso.', 'green');
+                exibirListaMangas(usuarioGlobal.uid);
+            } catch(error){
+                console.error('Error', error.message);
+                chamarPopUpAviso(error.message, 'red');
+            }
+            
+            formManga.reset();
+            formManga.style.display = 'none';
+            overlay.style.display = 'none';
+            abrirFormularioAux =! abrirFormularioAux;
         } else {
-            vetorMangas[emQualEsta] = novoManga;
-            emQualEsta = null;
+            chamarPopUpAviso('Nenhum usuário autenticado.', 'red');
         }
-
-        exibirListaMangas();
-        formManga.reset();
-        formManga.style.display = 'none';
-        overlay.style.display = 'none';
-        abrirFormularioAux =! abrirFormularioAux;
         
     });
-
     
     const botaoAbrirFormulario = document.getElementById('btn-abrir-form');
     let abrirFormularioAux = true;
